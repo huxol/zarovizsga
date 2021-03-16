c55e735a686d7d786f00097ef65458f1fc17cace
Huxol <huxol@freemail.hu>
2021-03-16 21:14:37 +0100

    Megoldás.

--- src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -1,6 +1,31 @@
 package hu.nive.ujratervezes.zarovizsga.dogtypes;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 public class DogTypes {
 
+    private DataSource dataSource;
 
+    public DogTypes(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> getDogsByCountry(String country) {
+        String countryByUpperCase = country.toUpperCase();
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt =
+                        conn.prepareStatement(" SELECT FROM DogTypes where Country = ?");
+        ) {
+            stmt.setLong(1, country);
+
+            // ...
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by insert", sqle);
+        }
+    }
 }
